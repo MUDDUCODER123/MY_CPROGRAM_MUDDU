333cf870e263e62113ebb5dd218e1e26cdfc899e
MUDDUCODER123 <mudduprasad53@gmail.com>
2025-11-18 16:07:18 +0530

    Create function making.c

---
+++ function making.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int username(){
+    int age;
+    char name,department;
+    printf("enter a name\n");
+    scanf("%s",&name);
+    printf("enter a age\n");
+    scanf("%d",&age);
+    printf("enter a departmentis\n");
+    scanf("%s",&department);
+}
+int main(){
+    username();{
+        
+    }
+}
