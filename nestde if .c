2bb7a1429a6fa1acd45fa4ca35d360abe4aabf7d
MUDDUCODER123 <mudduprasad53@gmail.com>
2025-11-18 12:15:31 +0530

    Create nestde if .c

---
+++ nestde if .c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num >= 0) {                     
+        printf("The number is positive.\n");
+
+        if (num % 2 == 0) {             
+            printf("It is even.\n");
+        }
+        else {  
+            printf("It is odd.\n");
+        }
+    }
+    else {                              
+        printf("The number is negative.\n");
+    }
+
+    return 0;
+}
