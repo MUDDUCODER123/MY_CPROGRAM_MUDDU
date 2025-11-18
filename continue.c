63cb2f4a2bd42f2b98fe57807d5deb9d275bd461
MUDDUCODER123 <mudduprasad53@gmail.com>
2025-11-18 12:02:38 +0530

    Create continue.c

---
+++ continue.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        if (i == 5) {
+            continue;   // Skip the rest of the loop when i is 5
+        }
+        printf("%d\n", i);
+    }
+}
