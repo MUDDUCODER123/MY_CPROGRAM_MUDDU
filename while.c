3d89e29dab8451112ef414360cfcb7e47845dc99
MUDDUCODER123 <mudduprasad53@gmail.com>
2025-11-18 11:48:52 +0530

    Create while.c

---
+++ while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    // Using a while loop to print numbers from 1 to 10
+    while (i <= 10) {
+        printf("%d\n", i);
+        i++;  // Incrementing the value of i by 1
+    }
+}
+    
