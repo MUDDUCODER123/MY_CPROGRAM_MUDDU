f03df9779bbd119fb7b9af0e95c58f5b0daae812
MUDDUCODER123 <mudduprasad53@gmail.com>
2025-11-21 14:07:12 +0530

    Create array of structure.c

---
+++ array of structure.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    struct muddu{
+      int age;
+      float marks;
+    };
+    struct muddu mp[3];
+    for(int i=0;i<3;i++){
+        printf("enter age & marks\n");
+        scanf("%d %f\n",&mp[i].age,mp[i].marks);
+        }
+    }
+    
+    
